6ad2e1e0cdd3396147a6d2fe497ab39ec32b26ff
Adam Blahuta <adam@jellyfishgames.ca>
2015-05-02 07:51:38 -0400

    * Added config changes

--- UnityProject/TCGameJam3DungeonCrawler/Assets/Scripts/CharacterController/CharacterMovementController.cs
+++ UnityProject/TCGameJam3DungeonCrawler/Assets/Scripts/CharacterController/CharacterMovementController.cs
@@ -31,12 +31,21 @@ public class CharacterMovementController : MonoBehaviour
     [SerializeField]
     private float minimumJumpTriggerHeight;
 
+    [SerializeField]
+    private float climbingSpeed;
+
     private Vector3 currentVelocity;
 
     private CharacterController characterController;
 
     private bool hasCharacterRequestedJumpWithinRange;
 
+    private bool isClimbing;
+    private bool startClimbing;
+    private Ladder currentLadder;
+
+    private int numLaddersClimbing;
+    
     private void Start()
     {
         this.characterController = GetComponent<CharacterController>();
@@ -46,19 +55,45 @@ public class CharacterMovementController : MonoBehaviour
     {
         Vector3 acceleration = new Vector3(0, -this.gravity, 0);
 
-        this.currentVelocity += acceleration * Time.deltaTime;
-        if (this.currentVelocity.magnitude > this.maxVerticalSpeed)
+        if(!this.isClimbing)
         {
-            this.currentVelocity = this.currentVelocity.normalized * this.maxVerticalSpeed;
+            this.currentVelocity += acceleration * Time.deltaTime;
+            if (this.currentVelocity.magnitude > this.maxVerticalSpeed)
+            {
+                this.currentVelocity = this.currentVelocity.normalized * this.maxVerticalSpeed;
+            }
         }
 
         HandleInput();
 
         this.characterController.Move(this.currentVelocity * Time.deltaTime);
     }
+    
+    public void HandleLadderEntry(Ladder ladder)
+    {
+        this.startClimbing = true;
+        this.currentLadder = ladder;
+
+        this.numLaddersClimbing++;
+    }
+
+    public void HandleLadderExit(Ladder ladder)
+    {
+        numLaddersClimbing--;
+
+        if(this.startClimbing)
+        {
+            this.startClimbing = false;
+        }
+
+        if (this.isClimbing && numLaddersClimbing == 0)
+        {
+            this.isClimbing = false;
+        }
+    }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
+    { 
         if( (this.characterController.collisionFlags & CollisionFlags.Above) != 0 ||
             (this.characterController.collisionFlags & CollisionFlags.Below) != 0)
         {
@@ -73,9 +108,27 @@ public class CharacterMovementController : MonoBehaviour
 
     private void HandleInput()
     {
+        if(this.startClimbing && Input.GetAxis("Vertical") > 0f)
+        {
+            this.startClimbing = false;
+            this.isClimbing = true;
+            this.currentVelocity = Vector3.zero;
+
+            Bounds ladderBounds = this.currentLadder.GetComponent<BoxCollider>().bounds;
+
+            float newHeight = this.characterController.isGrounded ? ladderBounds.center.y : this.transform.position.y;
+            this.transform.position = new Vector3(ladderBounds.center.x, newHeight, this.transform.position.z);
+        }
+
+        if(this.isClimbing)
+        {
+            float verticalInput = Input.GetAxis("Vertical");
+            this.currentVelocity.y = verticalInput * this.climbingSpeed;
+        }
+       
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        if(this.characterController.isGrounded)
+        if (this.characterController.isGrounded || this.isClimbing)
         {
             if (this.hasCharacterRequestedJumpWithinRange || Input.GetButtonDown("Jump"))
             {
@@ -85,17 +138,15 @@ public class CharacterMovementController : MonoBehaviour
         }
         else
         {
-            RaycastHit hitInfo;
-
-            if(!Input.GetButton("Jump"))
+            if (!Input.GetButton("Jump"))
             {
-                if(this.currentVelocity.y > this.jumpCutSpeedLimit)
+                if (this.currentVelocity.y > this.jumpCutSpeedLimit)
                 {
                     this.currentVelocity.y = this.jumpCutSpeedLimit;
                 }
             }
-            else if(Physics.Raycast(new Ray(this.transform.position, -this.transform.up), this.minimumJumpTriggerHeight, LayerMask.NameToLayer("Default")))
-            {   
+            else if (Physics.Raycast(new Ray(this.transform.position, -this.transform.up), this.minimumJumpTriggerHeight, LayerMask.NameToLayer("Default")))
+            {
                 this.hasCharacterRequestedJumpWithinRange = true;
             }
         }
@@ -108,11 +159,11 @@ public class CharacterMovementController : MonoBehaviour
         float horizontalAcceleration = horizontalInput * horizontalMovementSpeed * Time.deltaTime;
 
         this.currentVelocity += new Vector3(horizontalAcceleration, 0, 0);
-        //float maxHorizontalSpeed = this.characterController.isGrounded ? this.maxGroundSpeed : this.maxAirHorizontalSpeed;
         float maxHorizontalSpeed = this.maxGroundSpeed;
         this.currentVelocity = new Vector3(Mathf.Clamp(this.currentVelocity.x, -maxHorizontalSpeed, maxHorizontalSpeed),
-                                           Mathf.Clamp(this.currentVelocity.y, -this.maxVerticalSpeed, this.maxVerticalSpeed),
-                                           0f);
+                                            Mathf.Clamp(this.currentVelocity.y, -this.maxVerticalSpeed, this.maxVerticalSpeed),
+                                            0f);
+        
     }
 
     private void ApplyGroundFriction()
