7a205890fd9e312c4b21cfb1867ee0b8a58ed407
Adam Blahuta <adam@jellyfishgames.ca>
2015-05-02 16:59:40 -0400

    + Added missing weapon definitions

---
+++ UnityProject/TCGameJam3DungeonCrawler/Assets/Scripts/Weapons/Axe.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Axe : Weapon
+{
+    protected override void PointWeaponImpl(Vector3 direction) { }
+
+    protected override void AttackImpl(int totalDamage, Vector3 direction)
+    {
+
+    }
+}
---
+++ UnityProject/TCGameJam3DungeonCrawler/Assets/Scripts/Weapons/Whip.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Whip : Weapon
+{
+    protected override void PointWeaponImpl(Vector3 direction)
+    {
+
+    }
+
+    protected override void AttackImpl(int totalDamage, Vector3 direction)
+    {
+
+    }
+}
