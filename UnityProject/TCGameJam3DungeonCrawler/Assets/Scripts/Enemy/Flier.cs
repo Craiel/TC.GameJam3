e9b0d4f40272a5d87a840a0b06c44d8168dd1ebd
Adam Blahuta <adam@jellyfishgames.ca>
2015-05-02 14:39:07 -0400

    + Added generic Enemy behaviour flow + Added skeleton class for Flier

---
+++ UnityProject/TCGameJam3DungeonCrawler/Assets/Scripts/Enemy/Flier.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flier : Enemy
+{
+
+    protected override void Idle()
+    {
+        
+    }
+
+    protected override void Patrol()
+    {
+        
+    }
+
+    protected override void Chase()
+    {
+        
+    }
+
+    protected override void Attack()
+    {
+        
+    }
+}
