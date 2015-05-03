b225f20888424e3600c314f8fb4ada063886da28
Adam Blahuta <adam@jellyfishgames.ca>
2015-05-03 14:01:52 -0400

    + Added weapon UI flow

---
+++ UnityProject/TCGameJam3DungeonCrawler/Assets/Scripts/GameUIManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+
+public class GameUIManager : MonoBehaviour
+{
+    public Game game;
+    public Slider slider;
+
+    private int currentHealth;
+
+    void Update()
+    {
+        if (currentHealth != game.player.HitPoints)
+        {
+            slider.value = (float)game.player.HitPoints / game.player.TotalHitPoints;
+            currentHealth = game.player.HitPoints;
+        }
+    }
+}
